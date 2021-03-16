93713168b30b89731750731a4bfe96b0312c8971
Iva Istuk <ii51572@fer.hr>
2021-03-16 12:12:01 +0100

    Initilized backend

---
+++ backend/src/test/java/hr/demo/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.demo.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
